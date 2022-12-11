aa2db5eb9b9c8d2da00da1c987e2ef1e80dd1c31
Mark Grealish <mark@bhalash.com>
2022-12-11 17:27:51 +0000

    solve day one, problem two

---
+++ days/one/top_three_calories.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env ts-node
+// https://adventofcode.com/2022/day/1
+
+import * as readline from 'node:readline';
+
+const calories: number[] = [];
+let elfIndex = 0;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', line => {
+  const count = Number(line);
+
+  if (count === 0) {
+    elfIndex += 1;
+  } else {
+    calories[elfIndex] ||= 0;
+    calories[elfIndex] += count;
+  }
+});
+
+rl.once('close', () => {
+  console.log(
+    calories
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((acc, val) => acc + val, 0)
+  );
+});
